6b9a0b52dcd484953611ddd5665ca97d8d056517
datngovan <datngo.qlct@gmail.com>
2024-10-21 22:02:58 +0700

    Clean the code and abstract codes of convertFile function into smaller function IMP-004

--- lib/utils/convert.ts
+++ lib/utils/convert.ts
@@ -4,21 +4,55 @@ import { FFmpeg } from "@ffmpeg/ffmpeg"
 import { FfpmegCommandFactory } from "./command-factory"
 import { WorkerPool } from "./worker-pool"
 
+type convertOutput = {
+  url: string
+  output: string
+}
+type WorkerResultSuccess = {
+  success: true
+  outputFileName: string
+  log: string
+  outputFile: Uint8Array | string
+}
+type WorkerResultError = {
+  success: false
+  error: string
+}
+
+type WorkerResult = WorkerResultSuccess | WorkerResultError
+
+type FileData = Uint8Array | string
+/**
+ * Get the file extension.
+ * @param file_name File name.
+ * @returns File extension or empty string if not found.
+ */
 function getFileExtension(file_name: string): string {
   const regex = /(?:\.([^.]+))?$/ // Matches the last dot and everything after it
   const match = regex.exec(file_name)
-  return match && match[1] ? match[1] : "" // Return file extension or empty if none found
+  return match && match[1] ? match[1] : ""
 }
 
+/**
+ * Remove file extension.
+ * @param file_name File name.
+ * @returns File name without extension.
+ */
 function removeFileExtension(file_name: string): string {
   const lastDotIndex = file_name.lastIndexOf(".")
-  return lastDotIndex !== -1 ? file_name.slice(0, lastDotIndex) : file_name // Remove file extension if exists
+  return lastDotIndex !== -1 ? file_name.slice(0, lastDotIndex) : file_name
 }
 
+/**
+ * Convert file to other type.
+ * @param ffmpeg Wasm FFpmeg instance.
+ * @param action Action was pass in by user can be (image, audio, video).
+ * @returns A promise type convertOutput.
+ */
 export default async function convertFile(
   ffmpeg: FFmpeg,
   action: Action
-): Promise<{ url: string; output: string }> {
+): Promise<convertOutput> {
   const { to, file_name } = action
   const duration = await getFileDuration(ffmpeg, action)
   const fileChunks = await chunkFiles(ffmpeg, 5, duration, action)
@@ -29,47 +63,54 @@ export default async function convertFile(
   } else {
     outputFileName = removeFileExtension(file_name) + "." + to
   }
-  const workerPool = new WorkerPool(3)
-  workerPool.init()
-  const taskPromises = fileChunks.map((chunk, i) => {
-    const chunkInputFileName = `chunk_${i}.blob`
-    const chunkOutputFileName = `chunk_${i}.${to}`
-    const ffmpeg_cmd_chunk = FfpmegCommandFactory.getFfmpegCommand(
-      to ? to : "mp4",
-      chunkInputFileName,
-      chunkOutputFileName
-    )
-
-    return workerPool.addTask(async (worker) => {
-      // Convert the chunk using the worker
-      const result = await convertChunkWithWorker(worker, {
-        fileChunk: chunk,
-        inputName: chunkInputFileName,
-        outputName: chunkOutputFileName,
-        ffmpegCommand: ffmpeg_cmd_chunk,
-      })
-      if (result.success) {
-        // Write the output file (Uint8Array) back to FFmpeg's virtual file system
-        await ffmpeg.writeFile(result.outputFileName, result.outputFile)
-        return result.outputFileName // Return the output file name to be used later
-      } else {
-        throw new Error(`Chunk ${i + 1} conversion failed: ${result.error}`)
-      }
-    })
-  })
-
+  const taskPromises = assignWorkerToChunk(ffmpeg, fileChunks, to)
   // Wait for all tasks to finish and collect output file names
   const convertedChunks = await Promise.all(taskPromises)
   // Now concatenate the chunks using FFmpeg
   const fileListName = "file_list.txt"
-  let fileListContent = convertedChunks
+  const finalBlobData = await concatChunkToFinalFile(
+    fileListName,
+    convertedChunks,
+    ffmpeg,
+    outputFileName
+  )
+  const finalBlob = new Blob([finalBlobData], { type: `video/${to}` })
+  const url = URL.createObjectURL(finalBlob)
+
+  // Clean up intermediate files
+  for (const chunkFile of convertedChunks) {
+    await ffmpeg.deleteFile(chunkFile)
+  }
+  await ffmpeg.deleteFile(fileListName)
+
+  // Return the URL to the reassembled file
+  return { url, output: outputFileName }
+}
+
+/**
+ * Join the chunks to final file.
+ * @param fileListName Name of the file contain list of chunks name.
+ * @param chunksNameList List of the chunks name.
+ * @param ffmpeg FFmpeg instance.
+ * @param outputFileName Output file name.
+ * @returns A promise type File Data.
+ */
+async function concatChunkToFinalFile(
+  fileListName: string,
+  chunksNameList: string[],
+  ffmpeg: FFmpeg,
+  outputFileName: string
+): Promise<FileData> {
+  // Now concatenate the chunks using FFmpeg
+  let fileListContent = chunksNameList
     .map((chunk) => `file '${chunk}'`)
     .join("\n")
   await ffmpeg.writeFile(
     fileListName,
     new TextEncoder().encode(fileListContent)
   )
-
+  // Read the final concatenated file
+  let finalBlobData
   const concatCommand = [
     "-f",
     "concat",
@@ -97,28 +138,65 @@ export default async function convertFile(
     throw new Error("Chunk concatenation failed")
   }
 
-  // Read the final concatenated file
-  let finalBlobData
   try {
     finalBlobData = await ffmpeg.readFile(outputFileName)
   } catch (error) {
     console.error("Error reading concatenated file:", error)
     throw new Error("Error reading concatenated file")
   }
-
-  const finalBlob = new Blob([finalBlobData], { type: `video/${to}` })
-  const url = URL.createObjectURL(finalBlob)
-
-  // Clean up intermediate files
-  for (const chunkFile of convertedChunks) {
-    await ffmpeg.deleteFile(chunkFile)
-  }
-  await ffmpeg.deleteFile(fileListName)
-
-  // Return the URL to the reassembled file
-  return { url, output: outputFileName }
+  return finalBlobData
 }
 
+/**
+ * Assign worker to chunks
+ * @param ffmpeg FFmpeg instance.
+ * @param fileChunks File chunks array.
+ * @param outputFormat Format type of output file.
+ * @returns A promise name of the output file.
+ */
+
+function assignWorkerToChunk(
+  ffmpeg: FFmpeg,
+  fileChunks: Blob[],
+  outputFormat: string | undefined
+): Promise<string>[] {
+  const workerPool = new WorkerPool(3)
+  workerPool.init()
+  const taskPromises = fileChunks.map((chunk, i) => {
+    const chunkInputFileName = `chunk_${i}.blob`
+    const chunkOutputFileName = `chunk_${i}.${outputFormat}`
+    const ffmpeg_cmd_chunk = FfpmegCommandFactory.getFfmpegCommand(
+      outputFormat ? outputFormat : "mp4",
+      chunkInputFileName,
+      chunkOutputFileName
+    )
+
+    return workerPool.addTask(async (worker) => {
+      // Convert the chunk using the worker
+      const result = await convertChunkWithWorker(worker, {
+        fileChunk: chunk,
+        inputName: chunkInputFileName,
+        outputName: chunkOutputFileName,
+        ffmpegCommand: ffmpeg_cmd_chunk,
+      })
+      if (result.success) {
+        // Write the output file (Uint8Array) back to FFmpeg's virtual file system
+        await ffmpeg.writeFile(result.outputFileName, result.outputFile)
+        return result.outputFileName // Return the output file name to be used later
+      } else {
+        throw new Error(`Chunk ${i + 1} conversion failed: ${result.error}`)
+      }
+    })
+  })
+  return taskPromises
+}
+
+/**
+ * Get the durantion of the file if audio/video
+ * @param ffmpeg FFmpeg instance.
+ * @param action Action.
+ * @returns Duration of the file.
+ */
 async function getFileDuration(
   ffmpeg: FFmpeg,
   action: Action
@@ -169,17 +247,14 @@ async function getFileDuration(
   return totalDuration // Return the duration in seconds
 }
 
-async function listDirectoryContents(ffmpeg: FFmpeg, path: string = "/") {
-  try {
-    // List the directory contents
-    const directoryContents = await ffmpeg.listDir(path)
-    return directoryContents
-  } catch (error) {
-    console.error(`Error listing contents of directory "${path}":`, error)
-    return []
-  }
-}
-
+/**
+ * Chunk file into small chunk if audio/video
+ * @param ffmpeg FFmpeg instance.
+ * @param chunkDuration Chunk duration.
+ * @param totalDuration File duration.
+ * @param action Action.
+ * @returns TODO
+ */
 async function chunkFiles(
   ffmpeg: FFmpeg,
   chunkDuration: number,
@@ -262,20 +337,27 @@ async function chunkFiles(
   return chunks
 }
 /**
- * TODO later edit the data type
- * @param worker
- * @param data
+ * Convert a chunk using a worker and return the result.
+ * @param worker - The Worker instance to handle the conversion.
+ * @param data - The data to be sent to the worker for processing.
+ * @returns A Promise that resolves with the WorkerResult (either success or error).
  */
-function convertChunkWithWorker(worker: Worker, data: any): Promise<any> {
+function convertChunkWithWorker(
+  worker: Worker,
+  data: any
+): Promise<WorkerResult> {
   return new Promise((resolve, reject) => {
-    worker.onmessage = (e: MessageEvent) => {
-      const { success, outputFile, outputFileName, log } = e.data
+    worker.onmessage = (e: MessageEvent<WorkerResult>) => {
+      const { success } = e.data
 
       if (success) {
-        resolve({ success: true, outputFile, outputFileName })
+        resolve(e.data as WorkerResultSuccess)
       } else {
-        console.error("Chunk conversion failed:", e.data.error)
-        reject(new Error(e.data.error))
+        console.error(
+          "Chunk conversion failed:",
+          (e.data as WorkerResultError).error
+        )
+        reject(new Error((e.data as WorkerResultError).error))
       }
     }
 
